14075d952e8b6271041bb5d8fdaccf3f27c89618
foormalin <bv78dg@gmail.com>
2026-06-29 16:57:37 +0300

    Update create_marts.sql

--- sql/create_marts.sql
+++ sql/create_marts.sql
@@ -1,1 +1,8 @@
-
+CREATE TABLE IF NOT EXISTS sales_mart AS
+SELECT
+    customer_id,
+    COUNT(order_id) AS orders_count,
+    SUM(total_amount) AS total_revenue,
+    AVG(total_amount) AS avg_order_value
+FROM sales
+GROUP BY customer_id;
