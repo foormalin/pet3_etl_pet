5773429f166bc01443511dca0257a2c0d74f5451
foormalin <bv78dg@gmail.com>
2026-06-29 16:58:12 +0300

    Update analytical_queries.sql

--- sql/analytical_queries.sql
+++ sql/analytical_queries.sql
@@ -1,1 +1,13 @@
+SELECT
+    customer_id,
+    total_revenue
+FROM sales_mart
+ORDER BY total_revenue DESC
+LIMIT 10;
 
+SELECT
+    order_date,
+    SUM(total_amount) AS daily_revenue
+FROM sales
+GROUP BY order_date
+ORDER BY order_date;
