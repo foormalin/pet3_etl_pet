d34c6d7b8690e9714b11ce0c362cf76f24ff5b02
foormalin <bv78dg@gmail.com>
2026-06-29 16:57:04 +0300

    Update create_tables.sql

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,9 @@
-
+CREATE TABLE IF NOT EXISTS sales (
+    order_id INTEGER,
+    customer_id INTEGER,
+    product_id INTEGER,
+    order_date DATE,
+    quantity INTEGER,
+    price NUMERIC(10, 2),
+    total_amount NUMERIC(10, 2)
+);
